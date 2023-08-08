8105f4628d3fe355adafadf2cdd8c0bc2e8de862
andrey F de castro <andreycastro.564@gmail.com>
2023-08-08 14:30:21 -0300

    feat:creat store post function

--- js/script.post.js
+++ js/script.post.js
@@ -1,17 +1,48 @@
-function savePoste(){
-const title = document.getElementById("title").value;
-const category = document.getElementById("category").value;
-const resume = document.getElementById("resume").value;
-const autor = document.getElementById("autor").value;
-const date = document.getElementById("date").value;
+const posts = []
 
-console.log(title,category,resume,autor,Date);
+function savePoste() {
+    ;
 }
 
-function cleanfilde(){
+function cleanfilde() {
+    ;
+}
+
+if (title && category && resume && author && date)
+    storePost(title, category, resume, author, date);
+cleanfields();
+
+function storePost(title, category, resume, author, date) {
+    ;
+}
+
+
+function savePoste() {
+    const title = document.getElementById("title").value;
+    const category = document.getElementById("category").value;
+    const resume = document.getElementById("resume").value;
+    const autor = document.getElementById("autor").value;
+    const date = document.getElementById("date").value;
+
+    console.log(title, category, resume, autor, Date);
+}
+
+function cleanfilde() {
     document.getElementById("title").value = "";
     document.getElementById("category").value = "";
     document.getElementById("resume").value = "";
     document.getElementById("autor").value = "";
     document.getElementById("date").value = "";
 }
+
+function storePost(title, category, resume, author, date) {
+    const post = {
+        title,
+        category,
+        resume,
+        author,
+        date,
+    };
+}
+posts.push(post);
+console.log(post);
